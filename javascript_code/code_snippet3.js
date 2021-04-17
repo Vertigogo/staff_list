fccd7e6bf23c30a7c08d20e2539c993d85855611
Vertigogo <111134849+Vertigogo@users.noreply.github.com>
2021-04-17 19:29:50 +0000

    Added build instructions.

--- javascript_code/code_snippet3.js
+++ javascript_code/code_snippet3.js
@@ -1,1 +1,1 @@
-"use strict";var sjcl={cipher:{},hash:{},keyexchange:{},mode:{},misc:{},codec:{},exc
+"use strict";var sjcl={cipher:{},hash:{},keyexchange:{},mode:{},misc:{},codec:{},exception:{corrupt:function(a){this.toString=function(){return"CORRUPT: "+this.message};this.messag
