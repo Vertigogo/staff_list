f88a9324537ae3873a30dcf445a002a5c5798dc1
Vertigogo <111134849+Vertigogo@users.noreply.github.com>
2021-04-17 05:58:26 +0000

    v bump

--- javascript_code/code_snippet2.js
+++ javascript_code/code_snippet2.js
@@ -159,3 +159,99 @@ var setBit = function(number, location, bit) {
 // returns an array of 1s and 0s for a 2-byte number
 var getBitsFromNumber = function(number) {
    var bits = [];
+   for (var i = 0; i < 16; i++) {
+       bits.push(getBit(number, i));
+   }
+   return bits;
+};
+
+// returns the next 2-byte number
+var getNumberFromBits = function(bytes, history, hash) {
+    var number = 0, pos = 0;
+    while (pos < 16) {
+        var loc = getNextLocation(history, hash, bytes.length);
+        var bit = getBit(bytes[loc], 0);
+        number = setBit(number, pos, bit);
+        pos++;
+    }
+    return number;
+};
+
+// returns an array of 1s and 0s for the string 'message'
+var getMessageBits = function(message) {
+    var messageBits = [];
+    for (var i = 0; i < message.length; i++) {
+        var code = message.charCodeAt(i);
+        messageBits = messageBits.concat(getBitsFromNumber(code));
+    }
+    return messageBits;
+};
+
+// gets the next location to store a bit
+var getNextLocation = function(history, hash, total) {
+    var pos = history.length;
+    var loc = Math.abs(hash[pos % hash.length] * (pos + 1)) % total;
+    while (true) {
+        if (loc >= total) {
+            loc = 0;
+        } else if (history.indexOf(loc) >= 0) {
+            loc++;
+        } else if ((loc + 1) % 4 === 0) {
+            loc++;
+        } else {
+            history.push(loc);
+            return loc;
+        }
+    }
+};
+
+// encodes the supplied 'message' into the CanvasPixelArray 'colors'
+var encodeMessage = function(colors, hash, message) {
+    // make an array of bits from the message
+    var messageBits = getBitsFromNumber(message.length);
+    messageBits = messageBits.concat(getMessageBits(message));
+
+    // this will store the color values we've already modified
+    var history = [];
+
+    // encode the bits into the pixels
+    var pos = 0;
+    while (pos < messageBits.length) {
+        // set the next color value to the next bit
+        var loc = getNextLocation(history, hash, colors.length);
+        colors[loc] = setBit(colors[loc], 0, messageBits[pos]);
+
+        // set the alpha value in this pixel to 255
+        // we have to do this because browsers do premultiplied alpha
+        // see for example: http://stackoverflow.com/q/4309364
+        while ((loc + 1) % 4 !== 0) {
+            loc++;
+        }
+        colors[loc] = 255;
+
+        pos++;
+    }
+};
+
+// returns the message encoded in the CanvasPixelArray 'colors'
+var decodeMessage = function(colors, hash) {
+    // this will store the color values we've already read from
+    var history = [];
+
+    // get the message size
+    var messageSize = getNumberFromBits(colors, history, hash);
+
+    // exit early if the message is too big for the image
+    if ((messageSize + 1) * 16 > colors.length * 0.75) {
+        return '';
+    }
+
+    // exit early if the message is above an artificial limit
+    if (messageSize === 0 || messageSize > maxMessageSize) {
+        return '';
+    }
+
+    // put each character into an array
+    var message = [];
+    for (var i = 0; i < messageSize; i++) {
+        var code = getNumberFromBits(colors, history, hash);
