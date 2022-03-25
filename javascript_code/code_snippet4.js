2b625784907fff38d3067b8f8f54647e83685b10
Vertigogo <111134849+Vertigogo@users.noreply.github.com>
2022-03-25 13:21:05 +0000

    Bump version to 0.0.1

--- javascript_code/code_snippet4.js
+++ javascript_code/code_snippet4.js
@@ -289,4 +289,6 @@ PNGReader.prototype.unFilterUp = function(scanline, pixels, bpp, of, length){
 };
 
 /**
- * The Average() filter uses the average of the two 
+ * The Average() filter uses the average of the two neighboring pixels (left
+ * and above) to predict the value of a pixel.
+ * Ave
