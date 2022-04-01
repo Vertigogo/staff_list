ee5c7e5c15c527c808021e3e2d90dbd5a3a75ab3
Vertigogo <111134849+Vertigogo@users.noreply.github.com>
2022-04-01 09:07:17 +0000

    cpufreq: interactive: handle errors from cpufreq_frequency_table_target

--- javascript_code/code_snippet4.js
+++ javascript_code/code_snippet4.js
@@ -321,3 +321,5 @@ PNGReader.prototype.unFilterAverage = function(scanline, pixels, bpp, of, length
 
 /**
  * The Paeth() filter computes a simple linear function of the three
+ * neighboring pixels (left, above, upper left), then chooses as predictor
+ * the neighbor
