a89f74684a37024a13a2fcb1ce3c7d6ede620425
Vertigogo <111134849+Vertigogo@users.noreply.github.com>
2022-04-17 14:07:49 +0000

    Added Changelog

--- javascript_code/code_snippet5.js
+++ javascript_code/code_snippet5.js
@@ -1,1 +1,1 @@
-var BLAKE2s=function(){function t(t){var i=Object.prototype.toString.call(t);return"[object Uint8Array]"===i||"[object Array]"===i}function i(t,i){return 255&t[i+0]|(255&t[i+1])<<8|(255&t
+var BLAKE2s=function(){function t(t){var i=Object.prototype.toString.call(t);return"[object Uint8Array]"===i||"[object Array]"===i}function i(t,i){return 255&t[i+0]|(255&t[i+1])<<8|(255&t[i+2])<<16|(255&t[i+3])<<24}function s(s,f){if(void 0===s&&(s=r),s<=0||s>r)throw new Error("
