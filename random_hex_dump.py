5d5a179736582fc64ed67add5352bb18953e90ba
Vertigogo <111134849+Vertigogo@users.noreply.github.com>
2023-03-28 09:07:30 +0000

    Fixed Conflicts

--- random_hex_dump.py
+++ random_hex_dump.py
@@ -5,4 +5,6 @@ import binascii
 import random
 
 '''
-Generates a stream of random h
+Generates a stream of random hexidecimal characters
+'''
+parser = argparse.ArgumentParser(description='Process some in
