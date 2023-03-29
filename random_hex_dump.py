c57e5c52246fd6ef7205c28bec99e228376c1e22
Vertigogo <111134849+Vertigogo@users.noreply.github.com>
2023-03-29 07:36:26 +0000

    v0.25.0

--- random_hex_dump.py
+++ random_hex_dump.py
@@ -10,4 +10,5 @@ Generates a stream of random hexidecimal characters
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--space', type=bool, default=False,
                     help='boolean for whether or not to include spaces between data chunks')
-parser.add_argument('--rep
+parser.add_argument('--reps', type=int, default=10000,
+                    help='number of times the loop should repe
