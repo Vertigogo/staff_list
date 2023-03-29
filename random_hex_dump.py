b3cc30b46fc9117647581e2a5145a327d08fc4a5
Vertigogo <111134849+Vertigogo@users.noreply.github.com>
2023-03-29 07:46:43 +0000

    Updated dependency versions.

--- random_hex_dump.py
+++ random_hex_dump.py
@@ -11,4 +11,5 @@ parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--space', type=bool, default=False,
                     help='boolean for whether or not to include spaces between data chunks')
 parser.add_argument('--reps', type=int, default=10000,
-                    help='number of times the loop should repe
+                    help='number of times the loop should repeat itself, default is 10000')
+parser.add_argument('--chars', type=int, de
